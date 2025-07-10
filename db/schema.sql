f4133d6bbc7058134b96006de3c6cfc0ded1e434
Arush Singh <113715134+arushsingh0604@users.noreply.github.com>
2025-07-10 22:01:21 +0530

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,13 @@
+CREATE DATABASE IF NOT EXISTS employee_db;
+
+USE employee_db;
+
+CREATE TABLE IF NOT EXISTS employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  skill VARCHAR(50),
+  location VARCHAR(50),
+  experience_years INT,
+  available BOOLEAN
+);
 
