681bdb56e28d0ebaa1f73831fc6084905116985b
Arush Singh <113715134+arushsingh0604@users.noreply.github.com>
2025-07-10 22:03:17 +0530

    Update setup_web.sh

--- install-scripts/setup_web.sh
+++ install-scripts/setup_web.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "Deploying web UI..."
+cat <<EOF > /var/www/html/index.html
+<!-- Replace this with the contents from web/index.html -->
+EOF
 
